0a98a4cf8d9dfb1c53f17c731630e402837a49a0
andrey zhunenko <andrey.zhunenko@mail.ru>
2020-05-24 23:33:08 +0800

    My project Hexlet_with_C++

---
+++ hexlet_file.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+void func_helloy(){
+
+string name = "Andrey";
+cout <<"Hello, my name is" <<endl;
+cout <<name <<endl;
+
+}
+
+
+void Into_2_system_number(string num){}
+
+int main(){
+
+func_helloy();
+
+
+
+
+
+cout << endl;
+return 0;
+}
+
